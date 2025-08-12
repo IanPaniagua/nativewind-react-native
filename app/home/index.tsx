3d87aee7d9368a792c09c05837807299fbf768fb
IanPaniagua <paniagua.ian.de@gmail.com>
2025-08-12 15:09:43 +0200

    buttons

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -9,23 +9,46 @@ const HomeScreen = () => {
     <SafeAreaView>
         <View className='px-10'>
 
-          {/* one way to do it */}
-          <Link href="/products" asChild>
-          <CustomButton className='mb-10' onPress={() => router.push('/products') }>Productos</CustomButton>
-          </Link>          
+         
 
           {/* one way to do it */}
           {/* <CustomButton onPress={() => router.push('/products') }>Productos</CustomButton> */}
           
-          <CustomButton className='mb-10' color={"secondary"}>Secondary</CustomButton>
+          <CustomButton 
+            className='mb-2'
+            color={"primary"}
+            onPress={() => router.push('/products')}
+           >
+            Productos
+          </CustomButton>
+
+          <CustomButton 
+            className='mb-2'
+            color={"secondary"}
+            onPress={() => router.push('/profile')}
+           >
+            Profile
+          </CustomButton>
           
-          <CustomButton variant={'text-only'}>Text-only-Button</CustomButton>
-          {/* we added the prop classname for us other clasname */}
-          <CustomButton variant={'text-only'} className='mb-10'>Text-only-Button</CustomButton>
+          <CustomButton 
+            className='mb-2'
+            color={"tertiary"}
+            onPress={() => router.push('/settings')}
+           >
+            Settings
+          </CustomButton>
+          
 
             {/* <Link className='mb-5' href="/products">Products</Link>
             <Link className='mb-5' href="/profile">Profile</Link>
             <Link className='mb-5' href="/settings">Settings</Link> */}
+          {/* one way to do it */}
+          <Link href="/products" asChild>
+          <CustomButton 
+            className='mb-10'
+            variant='text-only'
+            onPress={() => router.push('/products') }>Productos</CustomButton>
+          </Link> 
         </View>
     </SafeAreaView>
   )
